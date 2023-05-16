b170476242404d894bfbd5645640455518d39ec1
吴佳熙 <1006961384@qq.com>
2023-05-16 11:10:29 +0800

    删除无用import

--- store/src/test/java/cn/edu/fc/store/service/StoreServiceTest.java
+++ store/src/test/java/cn/edu/fc/store/service/StoreServiceTest.java
@@ -2,15 +2,12 @@ package cn.edu.fc.store.service;
 
 import cn.edu.fc.StoreApplication;
 import cn.edu.fc.javaee.core.exception.BusinessException;
-import cn.edu.fc.javaee.core.util.RedisUtil;
 import cn.edu.fc.service.StoreService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest(classes = StoreApplication.class)
